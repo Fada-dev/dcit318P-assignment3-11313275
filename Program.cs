b0d432cd072a9a40b8deec6c4278d189a10208aa
Fada-dev <anthonyaffulkwabenabroni@gmail.com>
2025-08-15 09:22:44 +0000

    Implement Warehouse Inventory Management System (Q3)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Assignment3.Q1;
 using Assignment3.Q2;
+using Assignment3.Q3;
 
 namespace Assignment3
 {
@@ -8,7 +9,8 @@ namespace Assignment3
         public static void Main()
         {
             //new FinanceApp().Run();   // Run Question 1
-            new HealthSystemApp().Run(); // For Q2
+            //new HealthSystemApp().Run(); // For Q2
+            new WareHouseManager().Run();
 
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
